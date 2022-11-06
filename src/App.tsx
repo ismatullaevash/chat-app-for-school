9070ba9b30d6b0c3b47112453df8fe8181bf5b34
ismatullaevash <ismatullaeva.sh@gmail.com>
2022-11-05 22:12:12 -0400

    adding dummy frontned

---
+++ src/App.tsx
@@ -0,0 +1,194 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
+import "./App.css";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  BasicStorage,
+  ChatMessage,
+  ChatProvider,
+  Conversation,
+  ConversationId,
+  ConversationRole,
+  IStorage,
+  MessageContentType,
+  Participant,
+  Presence,
+  TypingUsersList,
+  UpdateState,
+  User,
+  UserStatus,
+} from "@chatscope/use-chat";
+import EmojiPicker from "emoji-picker-react";
+import { ExampleChatService } from "@chatscope/use-chat/dist/examples";
+import { Chat } from "./components/Chat";
+import { nanoid } from "nanoid";
+import { Col} from "react-bootstrap";
+import {
+  akaneModel,
+  eliotModel,
+  emilyModel,
+  joeModel,
+  users,
+} from "./data/data";
+import { AutoDraft } from "@chatscope/use-chat/dist/enums/AutoDraft";
+
+
+// sendMessage and addMessage methods can automagically generate id for messages and groups
+// This allows you to omit doing this manually, but you need to provide a message generator
+// The message id generator is a function that receives message and returns id for this message
+// The group id generator is a function that returns string
+const messageIdGenerator = (message: ChatMessage<MessageContentType>) =>
+  nanoid();
+const groupIdGenerator = () => nanoid();
+
+const akaneStorage = new BasicStorage({ groupIdGenerator, messageIdGenerator });
+const eliotStorage = new BasicStorage({ groupIdGenerator, messageIdGenerator });
+const emilyStorage = new BasicStorage({ groupIdGenerator, messageIdGenerator });
+const joeStorage = new BasicStorage({ groupIdGenerator, messageIdGenerator });
+
+// Create serviceFactory
+const serviceFactory = (storage: IStorage, updateState: UpdateState) => {
+  return new ExampleChatService(storage, updateState);
+};
+
+const akane = new User({
+  id: akaneModel.name,
+  presence: new Presence({ status: UserStatus.Available, description: "" }),
+  firstName: "",
+  lastName: "",
+  username: akaneModel.name,
+  email: "",
+  avatar: akaneModel.avatar,
+  bio: "",
+});
+
+const emily = new User({
+  id: emilyModel.name,
+  presence: new Presence({ status: UserStatus.Available, description: "" }),
+  firstName: "",
+  lastName: "",
+  username: emilyModel.name,
+  email: "",
+  avatar: emilyModel.avatar,
+  bio: "",
+});
+
+const eliot = new User({
+  id: eliotModel.name,
+  presence: new Presence({ status: UserStatus.Available, description: "" }),
+  firstName: "",
+  lastName: "",
+  username: eliotModel.name,
+  email: "",
+  avatar: eliotModel.avatar,
+  bio: "",
+});
+
+const joe = new User({
+  id: joeModel.name,
+  presence: new Presence({ status: UserStatus.Available, description: "" }),
+  firstName: "",
+  lastName: "",
+  username: joeModel.name,
+  email: "",
+  avatar: joeModel.avatar,
+  bio: "",
+});
+
+const chats = [
+  { name: "Akane", storage: akaneStorage },
+  { name: "Eliot", storage: eliotStorage },
+  { name: "Emily", storage: emilyStorage },
+  { name: "Joe", storage: joeStorage },
+];
+
+function createConversation(id: ConversationId, name: string): Conversation {
+  return new Conversation({
+    id,
+    participants: [
+      new Participant({
+        id: name,
+        role: new ConversationRole([]),
+      }),
+    ],
+    unreadCounter: 0,
+    typingUsers: new TypingUsersList({ items: [] }),
+    draft: "",
+  });
+}
+
+// Add users and conversations to the states
+chats.forEach((c) => {
+  users.forEach((u) => {
+    if (u.name !== c.name) {
+      c.storage.addUser(
+        new User({
+          id: u.name,
+          presence: new Presence({
+            status: UserStatus.Available,
+            description: "",
+          }),
+          firstName: "",
+          lastName: "",
+          username: u.name,
+          email: "",
+          avatar: u.avatar,
+          bio: "",
+        })
+      );
+
+      const conversationId = nanoid();
+
+      const myConversation = c.storage
+        .getState()
+        .conversations.find(
+          (cv) =>
+            typeof cv.participants.find((p) => p.id === u.name) !== "undefined"
+        );
+      if (!myConversation) {
+        c.storage.addConversation(createConversation(conversationId, u.name));
+
+        const chat = chats.find((chat) => chat.name === u.name);
+
+        if (chat) {
+          const hisConversation = chat.storage
+            .getState()
+            .conversations.find(
+              (cv) =>
+                typeof cv.participants.find((p) => p.id === c.name) !==
+                "undefined"
+            );
+          if (!hisConversation) {
+            chat.storage.addConversation(
+              createConversation(conversationId, c.name)
+            );
+          }
+        }
+      }
+    }
+  });
+});
+
+function App() {
+  const [value, setValue] = useState("Search....");
+  return (
+    <div className="h-100 d-flex flex-column overflow-hidden">
+        <Col>
+          <ChatProvider
+            serviceFactory={serviceFactory}
+            storage={akaneStorage}
+            config={{
+              typingThrottleTime: 250,
+              typingDebounceTime: 900,
+              debounceTyping: true,
+              autoDraft: AutoDraft.Save | AutoDraft.Restore,
+            }}
+          >
+            <Chat user={akane} />
+          </ChatProvider>
+        </Col>
+    </div>
+  );
+}
+
+export default App;
